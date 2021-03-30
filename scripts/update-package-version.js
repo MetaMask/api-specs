a7853697dfa4f5250728d9286657fb558da86ad5
shanejonas <jonas.shane@gmail.com>
2021-03-30 13:12:29 -0700

    fix: add workflow for release

---
+++ scripts/update-package-version.js
@@ -0,0 +1,21 @@
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
+
+const promisifiedFsReadFile = util.promisify(fs.readFile);
+const promisifiedFsWriteFile = util.promisify(fs.writeFile);
+
+const changeVersion = async (filePath, version) => {
+  const file = await promisifiedFsReadFile(filePath);
+  const json = JSON.parse(file.toString());
+  json.version = version;
+  return promisifiedFsWriteFile(filePath, JSON.stringify(json, null, 4));
+};
+const p = path.resolve(__dirname, "..", "package.json");
+
+changeVersion(p, process.env.NEW_VERSION).then(() => {
+    console.log("wrote new version")
+}).catch((err) => {
+    console.error("error writing new version")
+    console.error(err);
+});
