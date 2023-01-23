f527e2000bfdae49bdde6eeee1fa278f1574909d
Shane Jonas <jonas.shane@gmail.com>
2023-01-23 13:03:54 -0500

    Changed OpenRPC Document to be one file instead of many
    This simplifies the build and contributions

--- merge-openrpc.js
+++ merge-openrpc.js
@@ -1,14 +1,14 @@
-const MetaMaskOpenRPC = require("./metamask-openrpc.json");
+const MetaMaskOpenRPC = require("./openrpc.json");
 const fs = require("fs");
 const fetch = require("node-fetch");
 
 // fetch, merge and write
-fetch("https://raw.githubusercontent.com/etclabscore/ethereum-json-rpc-specification/master/openrpc.json")
+fetch("https://raw.githubusercontent.com/ethereum/execution-apis/assembled-spec/refs-openrpc.json")
   .then((res) => {
     return res.json();
   })
   .then((EthereumOpenRPC) => {
-    fs.writeFileSync(__dirname + "/GENERATED-openrpc.json",
+    fs.writeFileSync(__dirname + "/src/build/openrpc.json",
       JSON.stringify(
         mergeOpenRPC(MetaMaskOpenRPC, EthereumOpenRPC),
         null,
@@ -26,8 +26,34 @@ const mergeOpenRPC = (first, second) => {
       ...second.methods
     ],
     components: {
-      ...first.components,
-      ...second.components
+      errors: {
+        ...first.components.errors,
+        ...second.components.errors
+      },
+      schemas: {
+        ...first.components.schemas,
+        ...second.components.schemas
+      },
+      tags: {
+        ...first.components.tags,
+        ...second.components.tags
+      },
+      contentDescriptors: {
+        ...first.components.contentDescriptors,
+        ...second.components.contentDescriptors
+      },
+      examplePairings: {
+        ...first.components.examplePairings,
+        ...second.components.examplePairings
+      },
+      links: {
+        ...first.components.links,
+        ...second.components.links
+      },
+      examples: {
+        ...first.components.examples,
+        ...second.components.examples
+      }
     }
   }
 };
