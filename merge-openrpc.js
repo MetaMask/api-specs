8aef4d666ddc33d3ab32edb01786466b0d8903ce
Shane Jonas <jonas.shane@gmail.com>
2023-01-23 17:29:29 -0500

    Changed merge-openrpc to filter out unneeded methods

--- merge-openrpc.js
+++ merge-openrpc.js
@@ -2,20 +2,38 @@ const MetaMaskOpenRPC = require("./openrpc.json");
 const fs = require("fs");
 const fetch = require("node-fetch");
 
+
+const getFilteredExecutionAPIs = () => {
+  return fetch("https://raw.githubusercontent.com/ethereum/execution-apis/assembled-spec/refs-openrpc.json")
+    .then(async (res) => {
+      return filterExecutionAPIs(await res.json());
+    })
+}
+
 // fetch, merge and write
-fetch("https://raw.githubusercontent.com/ethereum/execution-apis/assembled-spec/refs-openrpc.json")
-  .then((res) => {
-    return res.json();
-  })
-  .then((EthereumOpenRPC) => {
-    fs.writeFileSync(__dirname + "/src/build/openrpc.json",
-      JSON.stringify(
-        mergeOpenRPC(MetaMaskOpenRPC, EthereumOpenRPC),
-        null,
-        4
-      )
-    );
+getFilteredExecutionAPIs().then((EthereumOpenRPC) => {
+  fs.writeFileSync(__dirname + "/src/build/openrpc.json",
+    JSON.stringify(
+      mergeOpenRPC(MetaMaskOpenRPC, EthereumOpenRPC),
+      null,
+      4
+    )
+  );
+})
+
+const unneeded = [
+  /eth_signTransaction/,
+  /eth_sign/,
+  /debug_.*/,
+];
+
+const filterExecutionAPIs = (openrpcDocument) => {
+  openrpcDocument.methods = openrpcDocument.methods.filter((method) => {
+    const matches = unneeded.some((regex) => regex.test(method.name))
+    return !matches;
   });
+  return openrpcDocument;
+}
 
 const mergeOpenRPC = (first, second) => {
   return {
